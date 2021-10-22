a7738528a12689a183abc7271b0d64fa3dfc308d
Alexey Zezin <alexey.zezin@gmail.com>
2021-10-22 15:26:33 +0300

    init: better vue structure and routing

---
+++ src/views/OnlineVN.vue
@@ -0,0 +1,17 @@
+<template>
+    <base-header/>
+    <router-view/>
+    <base-footer/>
+</template>
+
+<script>
+import BaseFooter from '../components/BaseFooter.vue'
+export default {
+  components: { BaseFooter },
+
+}
+</script>
+
+<style>
+
+</style>
