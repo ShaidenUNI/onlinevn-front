16b792667696494115a3a9f8adc487cbedd55389
Alexey Zezin <alexey.zezin@gmail.com>
2021-10-22 15:27:15 +0300

    init: better vue structure and routing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-
+  <online-vn/>
 </template>
 
 <style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Front page',
+    component: FrontPage
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/library',
+    name: 'Novel library',
+    component: NovelLibrary
+  },
+  {
+    path: '/novel/:id',
+    name: 'Novel info',
+    component: NovelInfo
+  },
+  {
+    path: '/editor/:id',
+    name: 'Editor',
+    component: Editor
+  },
 ]
 
 const router = createRouter({
