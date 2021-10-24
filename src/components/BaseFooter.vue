e9a976541771d2739244653a35990ec05345d47f
Alexey Zezin <alexey.zezin@gmail.com>
2021-10-24 11:53:20 +0300

    feat: grid and dynamic data setup

--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -1,21 +1,39 @@
 <template>
     <div class="footer">
-        Base footer
+        <div class="stats">
+            Статистика
+        </div>
+        <div class="media">
+            Медиа
+        </div>
+        <div class="extra">
+            Условия пользования
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    data () {
+    data() {
         return {
-
+            novelCount: 37,
+            assetCount: 451,
+            frameCount: 1598,
         }
-    }
+    },
 }
 </script>
 
-<style>
+<style scoped>
 .footer {
     margin-top: auto;
+    background-color: #232323;
+    border-top: 2px solid #545454;
+    padding: 70px 0;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 10px;
+    place-items: center;
 }
 </style>
